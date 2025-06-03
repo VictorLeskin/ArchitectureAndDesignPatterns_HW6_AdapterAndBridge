8cfb15191ee661968c7f99bf0a9a1d9e63fd2487
Victor Leskin <victorleskin16@gmail.com>
2025-06-03 07:54:05 +0300

    Add classes and function to parse source file and generate adapters

--- source/cgenerateadapter.cpp
+++ source/cgenerateadapter.cpp
@@ -1,4 +1,69 @@
 ///************************* ITELMA SP ****************************************
 
 #include "cgenerateadapter.hpp"
+#include <fstream>
 
+std::tuple<int, std::string> cGenerateAdapter::main(int argc, const char* argv[])
+{
+  if (argc != 2)
+    return std::tuple(1, "Missed interface header file\nUsage:[interface header file].");
+  
+  std::ifstream strm(argv[1]);
+  std::string report;
+
+  if( false == strm.fail() )
+  {
+    cInterfaceFileReader ifr(strm);
+    cAdapterClassesGenerator acg;
+    cAdapterClassesSourceFile acsf(argc, argv);
+
+    try
+    {
+      ifr.read();
+
+      if (false == ifr.empty())
+      {
+        while (auto iClass = ifr.getClass())
+        {
+          auto ic = acg.create(*iClass);
+          acsf.write(ic);
+
+          // init report string or add comma to separate class names
+          report = (report == "") ? std::string("Generated adapter for classes: ") : (report + ", ");
+          report += ic.ClassName();
+        }
+      }
+      else
+        return std::tuple(2, "No interface class found.");
+    }
+    catch (const std::exception& expected)
+    {
+      return std::tuple(3, std::string( "Something is wrong: ") + expected.what());
+    }
+  }
+  else 
+    return std::tuple(1, std::string( "Can't open input file") + argv[1] );
+    
+  return std::tuple(0,"");
+}
+
+void cInterfaceFileReader::read()
+{
+  throw std::exception("not imiplemented");
+}
+
+cAdapterClass cAdapterClassesGenerator::create(const cInterfaceClass&)
+{
+  throw std::exception("not imiplemented");
+  return cAdapterClass();
+}
+
+cAdapterClassesSourceFile::cAdapterClassesSourceFile(int argc, const char* argv[])
+{
+  throw std::exception("not imiplemented");
+}
+
+void cAdapterClassesSourceFile::write(const cAdapterClass&)
+{
+  throw std::exception("not imiplemented");
+}
--- source/cgenerateadapter.hpp
+++ source/cgenerateadapter.hpp
@@ -2,8 +2,83 @@
 #ifndef CGENERATEADAPTER_HPP
 #define CGENERATEADAPTER_HPP
 
+#include <tuple>
+#include <string>
+
+#include "ccppfunctiondeclarationparser.hpp"
+
+// This class generates adapters from special formatted interface classes.
+// The main function of the class opens a file, looks a interface classes and 
+// generates adapter classes for found interfaces.
 class cGenerateAdapter
 {
+public:
+  // return 0  if successful otherwise nonzer and a message
+  static std::tuple<int, std::string> main(int argc, const char* argv[]);
 };
+
+class cInterfaceClass
+{
+public:
+  std::string ClassName() const { return className; }
+
+protected:
+  std::string className;
+  std::vector<sParserResult> functions;
+};
+
+class cAdapterClass
+{
+public:
+  std::string ClassName() const { return interfaceClass->ClassName();  }
+
+protected:
+  cInterfaceClass* interfaceClass;
+};
+
+class cInterfaceFileReader
+{
+private:
+  cInterfaceFileReader(const cInterfaceFileReader&);
+  cInterfaceFileReader &operator=(const cInterfaceFileReader&);
+
+public:
+  cInterfaceFileReader(std::istream& s) : strm(s) {}
+
+  void read();
+
+  bool empty() const { return interfaceClasses.empty();  }
+  const cInterfaceClass* getClass()
+  {
+    return (interfaceClassesIdx < interfaceClasses.size())
+      ? &interfaceClasses[interfaceClassesIdx++] 
+      : nullptr;
+  }
+
+protected:
+  std::istream& strm;
+  int interfaceClassesIdx = 0;
+  std::vector<cInterfaceClass> interfaceClasses;
+};
+
+class cAdapterClassesGenerator 
+{
+public:
+  cAdapterClass create(const cInterfaceClass&);
+};
+
+class cAdapterClassesSourceFile
+{
+public:
+  cAdapterClassesSourceFile(int argc, const char* argv[]);
+
+  void write(const cAdapterClass &);
+
+protected:
+  void open();
+  void writeHeader();
+};
+
+
 
 #endif //#ifndef CGENERATEADAPTER_HPP
